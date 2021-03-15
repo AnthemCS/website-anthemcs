97587113f6114b3ad968105512f7dd47280417f7
Given Danguru <givendanguru@gmail.com>
2021-03-15 05:46:06 +0200

    initial commit

---
+++ src/entry.js
@@ -0,0 +1,12 @@
+import './js';
+import './css/index.scss';
+
+// Themekit
+// import './themekit/css/bootstrap-grid.css';
+// import './themekit/css/style.css';
+// import './themekit/css/glide.css';
+// import './themekit/css/magnific-popup.css';
+// import './themekit/css/content-box.css';
+// import './themekit/css/contact-form.css';
+// import './themekit/css/social.css';
+// import './themekit/css/skin.css';
---
+++ src/js/index.js
@@ -0,0 +1,23 @@
+console.log('Hello');
+
+
+{/* <script src="themekit/scripts/jquery.min.js"></script>
+<script src="themekit/scripts/main.js"></script>
+<script src="themekit/scripts/parallax.min.js"></script>
+<script src="themekit/scripts/glide.min.js"></script>
+<script src="themekit/scripts/progress.js"></script>
+<script src="themekit/scripts/tab-accordion.js"></script>
+<script src="themekit/scripts/magnific-popup.min.js"></script>
+<script src="themekit/scripts/pagination.min.js"></script>
+<script src="themekit/scripts/contact-form/contact-form.js"></script>
+<script src="themekit/scripts/custom.js"></script> */}
+
+import './scripts/main.js';
+import './scripts/parallax.min.js';
+import './scripts/glide.min.js';
+import './scripts/progress.js';
+import './scripts/tab-accordion.js';
+import './scripts/magnific-popup.min.js';
+import './scripts/pagination.min.js';
+import './scripts/contact-form/contact-form.js';
+import './scripts/custom.js';
